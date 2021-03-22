bf02bd022c91b21863ac27ea65913a6553f1fa56
sarahcabral <sarah.ly2009@gmail.com>
2021-03-21 22:05:45 -0300

    first commit

---
+++ src/main/java/com/atividades/ac1poo/Ac1PooApplication.java
@@ -0,0 +1,13 @@
+package com.atividades.ac1poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac1PooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac1PooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividades/ac1poo/Ac1PooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividades.ac1poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac1PooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
