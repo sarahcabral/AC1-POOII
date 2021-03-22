e0a5ea7d2783dcfff61f6c006641ff3c41e4f8d3
sarahcabral <sarah.ly2009@gmail.com>
2021-03-21 22:09:04 -0300

    criação Event

---
+++ src/main/java/com/atividades/ac1poo/entities/Event.java
@@ -0,0 +1,110 @@
+package com.atividades.ac1poo.entities;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Event {
+    
+    private Long id;
+    private String name;
+    private String description;
+    private String place;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String emailContact;
+    
+    public Event(Long id, String name, String description, String place, LocalDate startDate, LocalDate endDate,
+            LocalTime startTime, LocalTime endTime, String emailContact) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.place = place;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.emailContact = emailContact;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getEmailContact() {
+        return emailContact;
+    }
+
+    public void setEmailContact(String emailContact) {
+        this.emailContact = emailContact;
+    }
+
+    @Override
+    public String toString() {
+        return "Event [description=" + description + ", emailContact=" + emailContact + ", endDate=" + endDate
+                + ", endTime=" + endTime + ", id=" + id + ", name=" + name + ", place=" + place + ", startDate="
+                + startDate + ", startTime=" + startTime + "]";
+    }
+    
+}
