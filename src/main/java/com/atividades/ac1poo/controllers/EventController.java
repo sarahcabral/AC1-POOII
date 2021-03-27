b66bccaff327c4ec9394db3e10c194f0cdc76511
sarahcabral <sarah.ly2009@gmail.com>
2021-03-27 17:21:34 -0300

    BUSCA POR ID

--- src/main/java/com/atividades/ac1poo/controllers/EventController.java
+++ src/main/java/com/atividades/ac1poo/controllers/EventController.java
@@ -7,6 +7,7 @@ import com.atividades.ac1poo.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class EventController {
     public ResponseEntity<List<EventDTO>> getEvents() {
         List <EventDTO> list = service.getEvents();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<EventDTO> getEventById(@PathVariable Long id) {
+        EventDTO dto = service.getEventById(id);
+        return ResponseEntity.ok().body(dto);
     }
 
 }
--- src/main/java/com/atividades/ac1poo/services/EventService.java
+++ src/main/java/com/atividades/ac1poo/services/EventService.java
@@ -2,13 +2,16 @@ package com.atividades.ac1poo.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.atividades.ac1poo.dtos.EventDTO;
 import com.atividades.ac1poo.entities.Event;
 import com.atividades.ac1poo.repositories.EventRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class EventService {
@@ -28,5 +31,11 @@ public class EventService {
             listDTO.add(new EventDTO(e.getId(),e.getName(),e.getDescription(),e.getStartDate(),e.getEndDate(),e.getStartTime(),e.getEndTime()));
         }
         return listDTO;
+    }
+
+    public EventDTO getEventById(Long id) {
+        Optional<Event> op = repo.findById(id);
+        Event event = op.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Event not found"));
+        return new EventDTO(event);
     }
 }
